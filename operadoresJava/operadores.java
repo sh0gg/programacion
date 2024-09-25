5e2878dc6ce2e912d7402c61c79c43af90a6e9f6
sh0gg <davidmine800@gmail.com>
2024-09-25 09:41:06 +0200

    mecagooo

---
+++ operadoresJava/operadores.java
@@ -0,0 +1,75 @@
+package operadoresJava;
+
+public class operadores {
+    public static void main(String[] args) {
+        // Declaración de variables (ejemplos con tipo double)
+        double a = 1.0;
+        double b = 2.0;
+        double c = 3.0;
+        double d = 4.0;
+        double e = 5.0;
+        double f = 6.0;
+        double g = 7.0;
+        double h = 8.0;
+        double j = 9.0;
+        double m = 10.0;
+        double n = 11.0;
+        double p = 12.0;
+        double q = 13.0;
+        double r = 14.0;
+        double s = 15.0;
+        double x = 16.0;
+        double y = 17.0;
+        double z = 7.0;
+
+        // Operadores ejercicio 1
+        System.out.println(3.0 / 2.0 + 4.0 / 3.0); // a)
+
+        System.out.println(1.0 / (x - 5.0) + (3.0 * y) / 4.0); // b)
+
+        System.out.println(1.0 / 2.0 + 7.0); // c)
+
+        System.out.println(7.0 + 1.0 / 2.0); // d)
+
+        System.out.println((a * a) / (b - c) + (d - e) / (f - ((g * h) / j))); // e)
+
+        System.out.println((m / n) + p); // f)
+
+        System.out.println((m + n) / (p - q)); // g)
+
+        System.out.println((a * a) / (b * b) + (c * c) / (d * d)); // h)
+
+        System.out.println((m + n) / p / (q - (r / s))); // i)
+
+        System.out.println(((3.0 * a) + b) / (c - ((d + (5.0 * e)) / (f + (g / (2.0 * h)))))); // j)
+
+        System.out.println(((a * a) + (2.0 * a * b) + (b * b)) / (1.0 / (x * x) + 2.0)); // k)
+
+
+        // Operadores ejercicio 2
+        System.out.println((b / 2) - (4 * a * c)); // a)
+
+        System.out.println((3 * x * y) - (5 * x) + (12 * x) - 17); // b)
+
+        System.out.println((b + d) / (c + 4)); // c)
+
+        System.out.println(((x * y) / y) + 2); // d)
+
+        System.out.println((1 / y) + (3 * x / z) + 1); // e)
+
+        System.out.println((1 / (y + 3)) + (x / y) + 1); // f)
+
+        System.out.println((1 / y) + ((3 + x) / (y + 1))); // g)
+
+        // Ejercicio 3
+
+        a = 8;
+        b = 3;
+        c = -5;
+
+        // Operaciones
+
+        System.out.println(null);
+
+    }
+}
