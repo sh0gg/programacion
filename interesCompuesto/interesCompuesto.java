5edf20c82e3513048f16a962a14dbad5d3ae04a3
sh0gg <davidmine800@gmail.com>
2024-09-25 13:56:06 +0200

    añadir variable pagosPorAño

--- interesCompuesto/interesCompuesto.java
+++ interesCompuesto/interesCompuesto.java
@@ -12,14 +12,18 @@ public class interesCompuesto {
         System.out.print("Introduce el número de años que durará la inversión: ");
         int años = scanner.nextInt();
 
+        // System.out.print("Introduce el número de pagos por año: ");
+        // int pagosPorAño = scanner.nextInt();
+        int pagosPorAño = 1;
+
+        // Tasa de interés fija
         double tasaInteres = 0.045; // 4.5%
 
         // Calcular el importe final usando la fórmula del interés compuesto
-        double importeFinal = importe * Math.pow(1 + tasaInteres, años);
+        double importeFinal = importe * Math.pow(1 + (tasaInteres / pagosPorAño), pagosPorAño * años);
 
         System.out.printf("El importe final después de %d años es: %.2f euros%n", años, importeFinal);
 
         scanner.close();
     }
 }
-
