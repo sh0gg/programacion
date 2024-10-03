428ece7c56e4be6edc3c812dfeed273deb62015d
sh0gg <davidmine800@gmail.com>
2024-10-03 13:45:44 +0200

    changos 2, esta vez con armas

---
+++ java_project/DAM1 24-25 BRD/src/ud1/ejercicios/EP0113.java
@@ -0,0 +1,20 @@
+package ud1.ejercicios;
+
+/*EP0113. Dado el siguiente polinomio de segundo grado:
+y = ax^2 + bx + c
+crea un programa que pida los coeficientes a, b y c, así como el valor de x, y calcula el valor
+correspondiente de y.
+ */
+
+import java.util.Scanner;
+
+public class EP0113 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        
+
+        scanner.close();
+    }
+
+}
