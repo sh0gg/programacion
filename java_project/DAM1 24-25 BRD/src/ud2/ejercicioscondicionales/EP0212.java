823add969686111937b751fb3b53b2c72e086777
sh0gg <davidmine800@gmail.com>
2024-10-25 13:38:37 +0200

    asdsadsa

--- java_project/DAM1 24-25 BRD/src/ud2/ejercicioscondicionales/EP0212.java
+++ java_project/DAM1 24-25 BRD/src/ud2/ejercicioscondicionales/EP0212.java
@@ -9,14 +9,21 @@ import java.util.Scanner;
 
 public class EP0212 {
     public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
+
         System.out.print("Introduce el número de DNI (sin letra): ");
+
         int dni = sc.nextInt();
 
         char[] letras = { 'T', 'R', 'W', 'A', 'G', 'M', 'Y', 'F', 'P', 'D', 'X', 'B', 'N', 'J', 'Z', 'S', 'Q', 'V', 'H',
                 'L', 'C', 'K', 'E' };
+
         int indice = dni % 23;
+
         System.out.println("La letra correspondiente al DNI es: " + letras[indice]);
+
         sc.close();
+        
     }
 }
