e0de91d44252789d7b06f6ad47ba676ad71f0090
sh0gg <davidmine800@gmail.com>
2024-10-30 14:27:22 +0100

    laksjdsaldjsad

---
+++ java_project/DAM1 24-25 BRD/src/ud2/ejercicioscondicionales/base.java
@@ -0,0 +1,19 @@
+package ud2.ejercicioscondicionales;
+
+
+import java.util.Scanner;
+
+public class base {
+
+    public static void main(String[] args) {
+        
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("¿Cuáles son tus iniciales?");
+        String iniciales = scanner.nextLine();
+        System.out.println("¡Hola Mundo! Mi nombre es " + iniciales);
+
+        scanner.close();
+    }
+    
+}
