77982d20f40b910be4bd891beed7dff75f34eb09
sh0gg <davidmine800@gmail.com>
2024-11-06 14:21:02 +0100

    la llamada

--- java_project/DAM1 24-25 BRD/src/ud2/ejercicioscondicionales/EP0311.java
+++ java_project/DAM1 24-25 BRD/src/ud2/ejercicioscondicionales/EP0311.java
@@ -5,23 +5,34 @@ import java.util.Scanner;
 
 public class EP0311 {
     public static void main(String[] args) {
+        
         Scanner scanner = new Scanner(System.in);
         System.out.print("Introduce un número decimal: ");
         int decimal = scanner.nextInt();
 
         if (decimal == 0) {
+
             System.out.println("0");
+        
         } else {
+
             StringBuilder binario = new StringBuilder();
+
             while (decimal > 0) {
+
                 binario.append(decimal % 2);
                 decimal /= 2;
+
             }
            
             binario.reverse();
+
             for (int i = 0; i < binario.length(); i++) {
+
                 System.out.println(binario.charAt(i));
+                
             }
+
         }
 
         scanner.close();
