40f3ff74c6009609187d5e9ab896d2a7a7aeb2a7
sh0gg <davidmine800@gmail.com>
2024-11-13 13:31:42 +0100

    anthrax

---
+++ java_project/DAM1 24-25 BRD/src/ud2/cuatrocientos/ud2/cuatrocientos/EP0411.java
@@ -0,0 +1,29 @@
+package ud2.cuatrocientos;
+
+
+public class EP0411 {
+    public static int mcd(int a, int b) {
+
+        if (b == 0) {
+            return a;
+        }
+        if (a == 0) {
+            return b;
+        }
+        if (a >= b) {
+            return mcd(a - b, b);
+        } else {
+            return mcd(a, b - a);
+        }
+
+    }
+
+    public static void main(String[] args) {
+
+        int a = 56;
+        int b = 12;
+        
+        System.out.println("El MCD de " + a + " y " + b + " es: " + mcd(a, b));
+
+    }
+}
--- java_project/DAM1 24-25 BRD/src/xampp/xampp/src/App.java
+++
@@ -1,5 +0,0 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Ola Mundo!! \n Benvid@ ao repositorio de DAM1 do curso 24-25");
-    }
-}
