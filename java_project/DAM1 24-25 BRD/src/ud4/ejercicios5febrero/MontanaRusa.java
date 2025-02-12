a41b596e39f3e3f9451abf75a72b561cd6d39b77
sh0gg <davidmine800@gmail.com>
2025-02-12 08:21:22 +0100

    robespierre

--- java_project/DAM1 24-25 BRD/src/ud4/ejercicios5febrero/MontanaRusa.java
+++ java_project/DAM1 24-25 BRD/src/ud4/ejercicios5febrero/MontanaRusa.java
@@ -4,23 +4,28 @@ import java.util.Scanner;
 
 public class MontanaRusa {
     public static void main(String[] args) {
-        int[] montañaRusa1 = new int[]{4, 10, 3, 2};
-        int[] montañaRusa2 = new int[]{10, 3, 2, 4};
-        int[] montañaRusa3 = new int[]{4, 10, 3, 2};
-        int[] montañaRusa4 = new int[]{1, 2, 3, 2, 1, 0, 1, 2, 3, 2, 1};
+        int[] montanaRusa1 = new int[]{4, 10, 3, 2};
+        int[] montanaRusa2 = new int[]{10, 3, 2, 4};
+        int[] montanaRusa3 = new int[]{4, 10, 3, 2};
+        int[] montanaRusa4 = new int[]{1, 2, 3, 2, 1, 0, 1, 2, 3, 2, 1};
+
+        System.out.println(contarPicos(montanaRusa1));
+        System.out.println(contarPicos(montanaRusa2));
+        System.out.println(contarPicos(montanaRusa3));
+        System.out.println(contarPicos(montanaRusa4));
 
     }
-    int contarPicos(int[] alturas) {
-        int contadorPicos = 0;
-        for (int i = 0; i < alturas.length; i++) {
-            if (alturas[i] == alturas[0] && alturas[i] > alturas[alturas.length] && alturas[i] > alturas[i + 1]) {
-                contadorPicos++;
-            } else if  
-
-                if (alturas[i] > alturas[i-1] && alturas[i] > alturas[i+1]) {
-                contadorPicos++;
+    public static int contarPicos(int[] alturas) {
+            int contadorPicos = 0;
+            for (int i = 0; i < alturas.length; i++) {
+                if (alturas[i] == alturas[0] && alturas[i] > alturas[alturas.length] && alturas[i] > alturas[i + 1]) {
+                    contadorPicos++;
+                } else if (alturas[i] == alturas[alturas.length] && alturas[i] > alturas[0] && alturas[i] > alturas[i - 1] ) {
+                    contadorPicos++;
+                } else if (alturas[i] > alturas[i-1] && alturas[i] > alturas[i+1]) {
+                    contadorPicos++;
+                }
             }
-        }
-        return contadorPicos;
+            return contadorPicos;
     }
 }
