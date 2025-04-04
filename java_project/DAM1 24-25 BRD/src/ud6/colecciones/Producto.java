46abf9eb9989a02c788c542fb2190c5127e7f3bf
sh0gg <davidmine800@gmail.com>
2025-04-04 21:08:59 +0200

    there you go

---
+++ java_project/DAM1 24-25 BRD/src/ud6/colecciones/Producto.java
@@ -0,0 +1,39 @@
+package ud6.colecciones;
+
+import java.util.Objects;
+
+public class Producto {
+    private String codigo;
+    private String nombre;
+
+    public Producto(String codigo, String nombre) {
+        this.codigo = codigo;
+        this.nombre = nombre;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    @Override
+    public String toString() {
+        return nombre + " - " + codigo;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Producto)) return false;
+        Producto producto = (Producto) o;
+        return Objects.equals(codigo, producto.codigo);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(codigo);
+    }
+}
